bc36d1438039bf68ab2ee1ecda9ede1143e4db9f
E-Burton <edwina.antoinette@gmail.com>
2021-06-06 06:50:40 -0600

    Adding cadditional text components, adjusting text/font formatting, and added image component.

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text1}>Hello, World</Text>
+      <Text>My name is Edwina!</Text>
+      <Text style={styles.text3}>This is my first React Native App</Text>
+      <Image style={styles.img} source={{ uri: 'https://images.unsplash.com/photo-1622952736534-4ca61f7992fb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80' }}></Image>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +20,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'pink'
   },
+  text1: {
+    textDecorationLine: "underline",
+    fontVariant: ["small-caps"]
+  },
+  text3: {
+    color: 'green',
+    fontWeight: "800"
+  },
+  img: {
+    width: 400,
+    height: 250,
+    marginTop: 15
+  }
 });
